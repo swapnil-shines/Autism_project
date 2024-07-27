67521599876821613bfd8883d15bfdfd57868c36
swapnil-shines <67657408+swapnil-shines@users.noreply.github.com>
2024-07-27 16:26:35 +0600

    Add files via upload

---
+++ video.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="video.js" defer></script>
+    <link rel="stylesheet" href="video.css">
+</head>
+<style>
+    .navbar {
+      background-color: #333;
+      overflow: hidden;
+      justify-content: space-between;
+    }
+    .navbar a {
+      float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+
+    .navbar a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+</style>
+<body>
+    
+    <h3 class="heading">Exercises</h3>
+    <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="about.php">About</a>
+  
+</div>
+
+<div class="container">
+   <div class="main-video">
+    <div class="video">
+    <video src="images/vdo1.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+   </div>
+   </div>
+<div class="video-list">
+    <div class="vid active">
+        <video src="images/vdo2.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+    </div>
+    <div class="vid">
+        <video src="images/vdo3.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+    </div>
+    <div class="vid">
+        <video src="images/vdo4.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+    </div>
+    <div class="vid">
+        <video src="images/video1.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+    </div>
+    <div class="vid">
+        <video src="images/video2.mp4" controls muted autoplay></video>
+    <h3 class="title">Motivating children with autism to exercise</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Yoga_  Table Pose.mp4" controls muted autoplay></video>
+    <h3 class="title">Yoga-Table Pose</h3>
+    </div>
+    <div class="vid">
+        <video src="images/5 Best Exercises For Kids With Autism - Autism Exercises.mp4" controls muted autoplay></video>
+    <h3 class="title">5 Best Exercises For Kids With Autism</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Agility Ladder Exercises Benefit Children with Autism.mp4" controls muted autoplay></video>
+    <h3 class="title">Agility Ladder Exercises Benefit Children with Autism</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Autism in Children-  Exercises to Help Calm the Body.mp4" controls muted autoplay></video>
+    <h3 class="title">xercises to Help Calm the Body</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Autistic Kids - 7 Fun Sensory Activities for Autistic Kids.mp4" controls muted autoplay></video>
+    <h3 class="title">7 Fun Sensory Activities for Autistic Kids</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Basketball Activities for Children with Autism_2.mp4" controls muted autoplay></video>
+    <h3 class="title">Basketball Activities</h3>
+    </div>
+    <div class="vid">
+        <video src="images/Exercise for Children with Autism- Model Me Movement™- Balance, Core, & Calming.mp4" controls muted autoplay></video>
+    <h3 class="title">Balance, Core, & Calming</h3>
+    </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
+
+
+
+
